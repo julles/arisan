4dff5e9c91c7b00ea4074da142015a660c27f327
Muhamad Reza Abdul Rohim <reza.wikrama3@gmail.com>
2016-03-05 01:57:44 +0700

    modif route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,12 +1,30 @@
 <?php
-
-
-Route::group(['middleware' => ['web']], function () {
-	
-
-    Route::get('/',function(){
+Route::get('/',function(){
     	return redirect(config('oblagio.backendUrl').'/'.config('oblagio.firstMenu').'/index');
-    });
-   	
-  	include __DIR__.'/oblagioRoutes.php'; 
 });
+
+Route::get('auth','Backend\AuthController@login');
+Route::post('auth','Backend\AuthController@postLogin');
+Route::get('auth/sign-out','Backend\AuthController@signOut');
+Route::get('auth/forgot-password','Backend\AuthController@forgotPassword');
+Route::post('auth/forgot-password','Backend\AuthController@postForgotPassword');
+Route::group(['prefix' => og()->backendUrl , 'middleware' => ['backend','auth']],function(){
+		
+		Route::get('/' , function(){
+			return redirect('auth');
+		});
+		Route::auth();
+		if(\Schema::hasTable('menus'))
+		{
+			foreach(og()->menu()->where('permalink','!=','#')->get() as $row)
+			{
+				$controllerPath = app_path('Http/Controllers/'.$row->controller.'.php');
+
+				if(file_exists($controllerPath))
+				{
+					Route::controller($row->permalink,$row->controller);
+				}
+			}
+		}
+});
+
